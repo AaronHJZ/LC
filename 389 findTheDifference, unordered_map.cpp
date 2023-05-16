1da03da1c94bd7a62babec55533581e0380ed1f6
Air <875235550@qq.com>
2023-05-16 16:19:18 +0800

    Add files via upload

---
+++ 389 findTheDifference, unordered_map.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) 
+	{
+		unordered_map<char, int> m;
+		
+		for (char c : s)
+			m[c]++;
+
+		for (char c : t)
+		{
+			m[c]--;
+			if (m[c] < 0)
+				return c;
+		}
+
+		return ' ';
+    }
+};
